d250cf6cd33aa3141a1957addb89a13ccdfbbd1e
Masihullah RUSHAN ZAMIR <roshanzamir2017@gmail.com>
2025-02-24 17:56:06 +0100

    Mise à jour de App.jsx et Main.jsx avec de nouvelles fonctionnalités

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,24 @@
+// main.jsx - Point d'entrée principal de l'application
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+
+// Import des styles globaux
+import './styles/index.css'
+import './styles/App.css'
+import './styles/Auth.css'
+import './styles/Navbar.css'
+import './styles/MobileNavigation.css'
+
+// Import des polices et icônes
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
