71eb1cd5f25c8c3370f69beff0bf14678581e144
Masihullah RUSHAN ZAMIR <roshanzamir2017@gmail.com>
2025-04-04 12:12:08 +0200

    Amélioration du footer avec commentaires détaillés et accessibilité renforcée

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,25 +1,33 @@
-import { Facebook, Twitter, Instagram, Youtube } from "react-feather"
-import styles from "../styles/Auth.module.css"; // Updated import
 
+
+import { Facebook, Twitter, Instagram, Youtube } from "react-feather";
+import styles from "../styles/Auth.module.css"; // Import des styles CSS
 
 function Footer() {
   return (
-    <footer className={styles.siteFooter}> {/* Use styles here */}
+    <footer className={styles.siteFooter}>
+      {/* Conteneur principal du footer */}
       <div className={styles.footerContent}>
+        {/* Section des icônes sociales */}
         <div className={styles.socialIcons}>
+          {/* Lien Twitter */}
           <a href="#" className={styles.socialIcon} aria-label="Twitter">
             <Twitter />
           </a>
+          {/* Lien Facebook */}
           <a href="#" className={styles.socialIcon} aria-label="Facebook">
             <Facebook />
           </a>
+          {/* Lien Instagram */}
           <a href="#" className={styles.socialIcon} aria-label="Instagram">
             <Instagram />
           </a>
+          {/* Lien YouTube */}
           <a href="#" className={styles.socialIcon} aria-label="Youtube">
             <Youtube />
           </a>
         </div>
+        {/* Texte du footer (copyright et liens légaux) */}
         <div className={styles.footerText}>
           © 2024 Brand, Inc. · Privacy · Terms · Sitemap
         </div>
@@ -28,4 +36,4 @@ function Footer() {
   );
 }
 
-export default Footer
+export default Footer;
